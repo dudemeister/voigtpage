1fcfaaefdda150eef88c5fb53901f76f703939f9
Ali Jelveh <jelveh@web.de>
2011-06-16 11:20:09 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voigtpage::Application.config.secret_token = '5e59987ab2a18c6569e126de24d941cfd7e4c0ad69ce669b26554c58dd96e35a3f721736f84f7bfd2e1fab90fcfde1ef7bc64ff708cfebbada26a5e5c2924fc8'
